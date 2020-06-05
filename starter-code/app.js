afdde62e586584bf08b119aac06adeb73a28b1a6
daviddicken <davidfromseattle@gmail.com>
2020-06-05 16:52:10 -0700

    finished multiplyArray function

--- starter-code/app.js
+++ starter-code/app.js
@@ -123,12 +123,27 @@ IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
+
 function multiplyArray(multArr) { //eslint-disable-line
+//debugger;
+  var multiplyTotal = 1;                //had to start at one or would always return 0 ;)
+
+  for (var i = 0; i < multArr.length; i++)
+    {
+      var total = multiply(multArr[i], multiplyTotal); 
+      multiplyTotal = total[0];                  
+    }
+
+  var multiplyArrayString = `The numbers ${multArr[0]},${multArr[1]},${multArr[2]} have a product of ${multiplyTotal}.`
+
+  var returnMultiplyArray = [multiplyTotal, multiplyArrayString];
+
+  return returnMultiplyArray; 
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
